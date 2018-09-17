34c393c27fffb30721bfae7c55cb2f7eea0e3512
jimmyduang <jimmyduang@users.noreply.github.com>
2018-09-17 15:01:18 +0800

    添加graceful
    添加graceful

--- dockertest/main.go
+++ dockertest/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"os"
 	"path/filepath"
-
-	"gopkg.in/gin-gonic/gin.v1"
+	"syscall"
+	"time"
 
 	"fmt"
+	"os/signal"
+
+	"gopkg.in/gin-gonic/gin.v1"
 
 	"database/sql"
 	"io/ioutil"
@@ -27,7 +31,33 @@ func main() {
 
 		c.JSON(http.StatusOK, &ul)
 	})
-	router.Run(":8000")
+	// router.Run(":8000")
+
+	srv := &http.Server{
+		Addr:    ":8000",
+		Handler: router,
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
 
 type UserList struct {
